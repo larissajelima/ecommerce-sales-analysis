a319fe4401ce1a861c5d07379c439763b3dca178
Larissa Jesus Lima <larissajelima@gmail.com>
2026-03-20 21:29:18 -0300

    Update faturamento_diario.sql

--- queries/faturamento_diario.sql
+++ queries/faturamento_diario.sql
@@ -1,6 +1,8 @@
-SELECT 
-data_pedido,
-SUM(valor_total) AS faturamento_dia
+-- Objetivo: calcular o faturamento total por dia
+
+SELECT
+    data_pedido,
+    ROUND(SUM(valor_total), 2) AS faturamento_diario
 FROM pedidos
 GROUP BY data_pedido
 ORDER BY data_pedido;
