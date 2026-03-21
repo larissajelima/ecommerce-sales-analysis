8c80bce379da70f6068506b7166a8f4e8b959c1c
Larissa Jesus Lima <larissajelima@gmail.com>
2026-03-20 21:09:12 -0300

    Update taxa_recompra.sql

--- queries/taxa_recompra.sql
+++ queries/taxa_recompra.sql
@@ -1,13 +1,18 @@
-WITH pedidos_por_cliente AS (
-SELECT
-cliente_id,
-COUNT(*) AS quantidade_pedidos
-FROM pedidos
-GROUP BY cliente_id
+WITH quantidade_pedidos AS (
+    SELECT
+        cliente_id,
+        COUNT(*) AS quantidade_pedidos
+    FROM pedidos
+    GROUP BY cliente_id
+),
+base AS (
+    SELECT
+        COUNT(*) AS total_clientes,
+        SUM(CASE WHEN quantidade_pedidos > 1 THEN 1 ELSE 0 END) AS clientes_recorrentes
+    FROM quantidade_pedidos
 )
-
 SELECT
-COUNT(*) AS total_clientes,
-SUM(CASE WHEN quantidade_pedidos > 1 THEN 1 ELSE 0 END) AS clientes_recorrentes,
-SUM(CASE WHEN quantidade_pedidos > 1 THEN 1 ELSE 0 END) * 1.0 / COUNT(*) AS taxa_recompra
-FROM pedidos_por_cliente;
+    total_clientes,
+    clientes_recorrentes,
+    ROUND(clientes_recorrentes * 100.0 / total_clientes, 2) AS taxa_recompra_percentual
+FROM base;
