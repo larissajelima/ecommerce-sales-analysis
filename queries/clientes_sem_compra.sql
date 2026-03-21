d73e3548e4b8141589c1902c8d604b859bed939e
Larissa Jesus Lima <larissajelima@gmail.com>
2026-03-20 21:29:59 -0300

    Update clientes_sem_compra.sql

--- queries/clientes_sem_compra.sql
+++ queries/clientes_sem_compra.sql
@@ -1,7 +1,10 @@
-SELECT 
-c.cliente_id,
-c.nome
+-- Objetivo: identificar clientes cadastrados que nunca realizaram compras
+
+SELECT
+    c.cliente_id,
+    c.nome,
+    c.cidade
 FROM clientes c
 LEFT JOIN pedidos p
-ON c.cliente_id = p.cliente_id
-WHERE p.pedido_id IS NULL;
+    ON c.cliente_id = p.cliente_id
+WHERE p.cliente_id IS NULL;
